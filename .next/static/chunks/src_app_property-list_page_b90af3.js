c40942665b05249194ecb479f60b501d7b47c033
piyushevdt <piyush.evdt@gmail.com>
2025-01-30 13:10:34 +0530

    New

---
+++ .next/static/chunks/src_app_property-list_page_b90af3.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_property-list_page_b90af3.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_property-list_page_b90af3.js",
+  "chunks": [
+    "static/chunks/src_7b81c6._.js",
+    "static/chunks/node_modules_3d768d._.js"
+  ],
+  "source": "dynamic"
+});
