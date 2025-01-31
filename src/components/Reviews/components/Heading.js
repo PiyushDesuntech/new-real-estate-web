829df8fc35236f849353b75700f88e606b9c6d96
Jitam-Bharadwaj <jitam.desuntechnology@gmail.com>
2025-01-31 17:16:03 +0530

    reviews entire page

--- src/components/Reviews/components/Heading.js
+++ src/components/Reviews/components/Heading.js
@@ -6,19 +6,20 @@ import Link from "next/link";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function Heading() {
-  const pathname = usePathname();
-  const pathSegments = pathname.split("/").filter(Boolean);
+  const pathname = usePathname(); // Get the current path
+  const pathSegments = pathname.split("/").filter(Boolean); // Split and filter empty segments
 
   const breadcrumbLinks = pathSegments.map((segment, index) => {
     const isLast = index === pathSegments.length - 1;
     const routePath = "/" + pathSegments.slice(0, index + 1).join("/");
 
+    // Capitalize the segment for display
     const displayName = segment
       .replace(/-/g, " ") 
       .replace(/\b\w/g, (char) => char.toUpperCase()); 
 
     return isLast ? (
-      <Typography key={routePath} color="text.primary">
+      <Typography key={routePath} sx={{ color: "#B3A87A", fontWeight: 400, fontSize: "21.67px" }}>
         {displayName}
       </Typography>
     ) : (
@@ -37,7 +38,7 @@ function Heading() {
   return (
    <Box sx={{ py: 4, px: {xs: 1, sm: 3}}}>
      <Breadcrumbs aria-label="breadcrumb" separator={<ArrowForwardIosIcon sx={{fontSize: "10px"}}/>}>
-      <MuiLink component={Link} href="/" underline="hover" color="inherit">
+      <MuiLink component={Link} href="/" underline="hover" color="inherit" sx={{ fontWeight: 400, fontSize: "21.67px" }}>
         Home
       </MuiLink>
       {breadcrumbLinks}
