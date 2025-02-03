0d29344cb5ba609d051719513000ab8b4f8c45c3
Jitam-Bharadwaj <jitam.desuntechnology@gmail.com>
2025-02-03 16:16:37 +0530

    image viewer complete

--- .next/static/chunks/src_app_properties-image-viewer_page_b90af3.js
+++ .next/static/chunks/src_app_properties-image-viewer_page_b90af3.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_properties-image-viewer_page_b90af3.js",
   "chunks": [
-    "static/chunks/_213a81._.js"
+    "static/chunks/_1086fd._.js"
   ],
   "source": "dynamic"
 });
