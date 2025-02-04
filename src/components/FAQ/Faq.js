93da222f016b474f9598e15be978c1c8a7239d4f
Jitam-Bharadwaj <jitam.desuntechnology@gmail.com>
2025-02-04 11:54:44 +0530

    FAQ Complete

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -8,10 +8,23 @@ import JoinUs from "../Home/compoments/JoinUs";
 export default function Faq() {
   return (
     <Box>
-      <Container maxWidth="xl" sx={{ minHeight: "100vh", pb: 10, px: {xs: 3, md: 4 } }}>
+      <Container maxWidth="xl" sx={{ minHeight: "100vh", pb: 10, px: {xs: 1, md: 1 } }}>
+      <Box
+        sx={{
+          paddingLeft: { xs: '0px', sm: '0px', lg: '0px' },
+          paddingTop: { xs: '24px', sm: '32px' },
+          marginBottom: { xs: '10px', sm: '10px', lg: '10px' },
+        }}
+      >
         <Heading />
+      </Box>
+      <Box sx={{
+          marginBottom: { xs: '1px', sm: '1px', md:'4px', lg: '5px' },
+          }}>
         <Frequently_asked_ques />
-        <Box sx={{px: {xs: 0, md: 4}}}>
+        </Box>
+        <Box sx={{
+          px: {xs: 0, md: 4}}}>
         <Questions_container />
         </Box>
       </Container>
